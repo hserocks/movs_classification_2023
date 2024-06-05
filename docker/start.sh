59e54a11f81b59fdfbc034e977f5035a90035b79
Nikolay <nasilaev@edu.hse.ru>
2024-06-05 22:51:45 +0100

    upd

--- docker/start.sh
+++ docker/start.sh
@@ -4,4 +4,5 @@
 nohup python bot_new.py > /dev/null 2>&1 &
 
 # Start API app
-exec gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+#exec gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+exec gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:9999
